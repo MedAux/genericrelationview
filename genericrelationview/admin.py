b6c893515161a7f0fe26c666933cb6acf2a30af0
zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2018-01-10 14:25:52 +0600

    Adapt `GenericAdminMixin` for Django 1.11
    Add argument `request` to methods `forfiled_for_dbfield` and `forfiled_for_content_type` as Django uses it to check user
    privileges.

--- genericrelationview/admin.py
+++ genericrelationview/admin.py
@@ -10,26 +10,26 @@ class GenericAdminMixin(object):
     def __init__(self, *args, **kwargs):
         super(GenericAdminMixin, self).__init__(*args, **kwargs)
 
-    def formfield_for_dbfield(self, db_field, **kwargs):
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
         save_kwargs = dict(kwargs)
-        request = kwargs.pop("request", None)
+        print('GenericAdminMixin', db_field, request)
         for (content_type, object_id) in self.generic_pairs:
             if db_field.name == content_type:
                 return self.formfield_for_content_type(
-                    db_field, object_id, content_type, **kwargs
+                    db_field, request, object_id, content_type, **kwargs
                 )
             elif db_field.name == object_id:
                 return self.formfield_for_object_id(db_field, **kwargs)
         return super(
             GenericAdminMixin, self
-        ).formfield_for_dbfield(db_field, **save_kwargs)
+        ).formfield_for_dbfield(db_field, request, **save_kwargs)
 
     def formfield_for_content_type(
-        self, db_field, object_id, content_type, **kwargs
+        self, db_field, request, object_id, content_type, **kwargs
     ):
         formfield = super(
             GenericAdminMixin, self
-        ).formfield_for_foreignkey(db_field, **kwargs)
+        ).formfield_for_foreignkey(db_field, request, **kwargs)
         widget = formfield.widget
         url = reverse('generickey_json')
         widget.attrs.update({
